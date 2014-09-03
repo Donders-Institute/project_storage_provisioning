45e02dba60d3372ea57fb06ffc8628af5d663c0a
Hurng-Chun Lee <h.lee@donders.ru.nl>
2014-09-03 14:16:42 +0200

    add first implementation for setting ACL

---
+++ setacl.py
@@ -0,0 +1,90 @@
+#!/mnt/opt/python-2.7.6/bin/python
+import sys
+import os 
+import logging
+from argparse import ArgumentParser
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from utils.ACL    import getACE, setACE, delACE
+from utils.Common import getMyLogger, csvArgsToList
+
+## set default logger format
+logging.basicConfig(format='[%(levelname)s:%(name)s] %(message)s')
+
+## execute the main program
+if __name__ == "__main__":
+
+    parg = ArgumentParser(description='sets/adds ACL of a path iteratively', version="0.1")
+
+    ## positional arguments
+    parg.add_argument('path',
+                      metavar = 'path',
+                      nargs   = '+',
+                      help    = 'the path')
+
+    ## optional arguments
+    parg.add_argument('-l','--loglevel',
+                      action  = 'store',
+                      dest    = 'verbose',
+                      type    = int,
+                      choices = [0, 1, 2, 3],  ## choices work only with str
+                      default = 0,
+                      help    = 'set one of the following verbosity levels. 0|default:WARNING, 1:ERROR, 2:INFO, 3:DEBUG')
+
+    parg.add_argument('-u','--user',
+                      action  = 'store',
+                      dest    = 'users',
+                      default = '',
+                      help    = 'set list of system uids separated by "," for the user role')
+
+    parg.add_argument('-c','--contributor',
+                      action  = 'store',
+                      dest    = 'contributors',
+                      default = '',
+                      help    = 'set list of system uids separated by "," for the contributor role')
+
+    parg.add_argument('-a','--admin',
+                      action  = 'store',
+                      dest    = 'admins',
+                      default = '',
+                      help    = 'set list of system uids separated by "," for the admin role')
+
+    args = parg.parse_args()
+
+    logger = getMyLogger(name=__file__, lvl=args.verbose)
+
+    args.admins        = args.admins.strip()
+    args.users         = args.users.strip()
+    args.contributors  = args.contributors.strip()
+
+    _l_admin   = csvArgsToList(args.admins) 
+    _l_user    = csvArgsToList(args.users) 
+    _l_contrib = csvArgsToList(args.contributors) 
+
+    ## there is no reason to set yourself for admin, user, contributor
+    ## since the you should have been the admin to run this program
+    ## and admin has all the rights.
+    ##  
+    ## if there is a need to change (downgrade) your permission, use
+    ## the super user via sudo to run this program.
+    try:
+        _l_admin.remove( os.environ['LOGNAME'] )
+    except ValueError, e:
+        pass
+
+    try:
+        _l_user.remove( os.environ['LOGNAME'] )
+    except ValueError, e:
+        pass
+
+    try:
+        _l_contrib.remove( os.environ['LOGNAME'] )
+    except ValueError, e:
+        pass
+
+    for p in args.path:
+        if os.path.exists(p):
+            logger.info('setting file or directory: %s' % p)
+            setACE(p, admins=_l_admin, users=_l_user, contributors=_l_contrib, lvl=args.verbose)
+        else:
+            logger.error('file or directory not found: %s' % p)
---
+++ utils/ACL.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+import logging
+from utils.Shell  import *
+from utils.Common import getMyLogger
+
+def get_permission(role):
+    ''' gets permission bits for DENY and ALLOW, given the role
+    '''
+    all_permission = 'rwaDdxnNtTcCoy'
+
+    _role_acl_ = { 'admin'      : 'RXWdDy',
+                   'contributor': 'rwdDtTnNcy',
+                   'user'       : 'RXy' }
+   
+    _alias_ = { 'R': 'rntcy',
+                'W': 'watTNcCy',
+                'X': 'xtcy' }
+ 
+    acl = {} 
+    try:
+        _acl_a = _role_acl_[role]
+
+        for k,v in _alias_.iteritems():
+            _acl_a = _acl_a.replace(k, v)
+
+        _acl_a = ''.join( list( set(list(_acl_a)) ) )
+        _acl_d = ''.join( list( set(list(all_permission)) - set(list(_acl_a)) ) )
+
+        acl['A'] = _acl_a
+        acl['D'] = _acl_d
+
+    except KeyError,e:
+        logger.error('No such role: %s' % role)
+
+    return acl
+
+def delACE(path, users, roles=None, lvl=0):
+    ''' deletes all ACE entry related to the given list of users.
+        If roles are gien, only the ACEs corresponding to the roles are deleted.
+    '''
+
+    logger = getMyLogger(lvl=lvl)
+
+    if not users:
+        return True
+
+    if not roles:
+        roles = ['admin','contributor','user']
+
+    _acls_perm = {}
+    for r in roles:
+        _acls_perm[r] = get_permission(r)
+
+    _acl  = []
+    _opts = ['-R','-x']
+    for u in users:
+        for r,p in _acls_perm.iteritems():
+
+            #if u == os.environ['LOGNAME'] and r == 'admin':
+            #    logger.warn('skip removing %s from admin role.' % u)
+            #    continue
+
+            _acl.append('D:fd:%s@dccn.nl:%s' % (u, p['D'])) 
+            _acl.append('A:fd:%s@dccn.nl:%s' % (u, p['A']))
+
+            ## add possible variations when permissions are propogate to files
+            _acl.append('D::%s@dccn.nl:%s' % (u, p['D'].replace('D',''))) 
+            _acl.append('A::%s@dccn.nl:%s' % (u, p['A'].replace('D','')))
+
+    for a in _acl:
+        logger.debug('delete ACE: %s' % a) 
+
+    return __nfs4_setfacl__(path, _acl, _opts)
+
+def getACE(path, user=None, recursive=False, lvl=0):
+    ''' gets ACEs for given user or for all ACEs if user is not given. 
+    '''
+
+    logger = getMyLogger(lvl=lvl)
+
+    def __nfs4_getfacl__(fpath):
+
+        logger.debug('get ACL of %s ...' % fpath)
+
+        cmd = 'nfs4_getfacl %s' % fpath 
+        rc, output, m = s.cmd1(cmd, allowed_exit=[0,255], timeout=300)
+        if rc != 0:
+            logger.error('%s failed' % cmd)
+            return None
+        else:
+            return __parseACLStr__(output)
+        
+    acl = {}
+    s   = Shell()
+    if recursive:
+        ## walk through all directories/files under the path
+        for r,ds,fs in os.walk(path,onerror=__fs_walk_error__):
+            ## retrieve directory ACL
+            acl[r] = __nfs4_getfacl__(r)
+ 
+            ## retrieve file ACL
+            for f in map(lambda x:os.path.join(r,x), fs):
+                acl[f] = __nfs4_getfacl__(f)
+    else:
+        acl[path] = __nfs4_getfacl__(path)
+
+    return acl
+
+def setACE(path, users=[], contributors=[], admins=[], lvl=0):
+    ''' adds/sets ACEs for user, contributor and admin roles.
+         - for user id not presented in current ACL, create a new ACE.
+         - for user id already presented in current ACL, update it by
+           removing followed by adding it back with correct right.
+    '''
+
+    logger = getMyLogger(lvl=lvl)
+
+    ## firstly check if same user id appears in three input lists.
+    ## if so, an error is thrown.
+    common = list( set(users) & set(contributors) & set(admins) )
+
+    if common:
+        for u in common:
+            logger.error('user %s presents in multiple roles.' % u)
+        return False
+
+    ## retrieve current ACEs of the top directory to check which users 
+    ## are presented in the current ACEs
+    _ulist_a = users + contributors + admins
+
+    ## if the entire user list is empty, just return true
+    if not _ulist_a:
+        return True
+
+    _ulist_e = []
+    aces = getACE(path, recursive=False, lvl=lvl)[path]
+    for a in aces:
+        u = a[2].split('@')[0]
+        if u in _ulist_a:
+            _ulist_e.append(u)
+
+    _ulist_e = list(set(_ulist_e))
+
+    if not delACE(path, _ulist_e, lvl=lvl):
+        logger.error('Unable to remove existing ACEs of users: %s' % ', '.join(_ulist_e) )
+        return False
+
+    ulist = {'user':users, 'contributor':contributors, 'admin':admins}
+    aces  = []
+    opts  = ['-R', '-a']
+    for k,v in ulist.iteritems():
+        logger.info('Setting %s permission ...' % k)
+        _perm = get_permission(k)
+        for u in v:
+            aces.append('D:fd:%s@dccn.nl:%s' % (u, _perm['D']))
+            aces.append('A:fd:%s@dccn.nl:%s' % (u, _perm['A']))
+
+    for a in aces:
+        logger.debug('add ACE: %s' % a)
+
+    return __nfs4_setfacl__(path, aces, opts)
+
+def __nfs4_setfacl__(fpath, aces, options=None):
+    ''' wrapper for calling nfs4_setfacl command.
+         - fpath  : the path the ACEs will be applied
+         - aces   : the ACEs in a list of strings
+         - options: the command-line options in a list of strings 
+    '''
+    if options:
+        cmd = 'nfs4_setfacl %s ' % ' '.join(options)
+    else:
+        cmd = 'nfs4_setfacl '
+
+    cmd += '"%s" %s' % ( ', '.join(aces), fpath )
+
+    s = Shell()
+    rc, output, m = s.cmd1(cmd, allowed_exit=[0,255], timeout=300)
+    if rc != 0:
+        logger.error('%s failed' % cmd)
+        return False 
+    else:
+        return True
+   
+def __fs_walk_error__(err):
+    ''' handles error if not able to perform listdir on a file.
+    '''
+    print 'cannot list file: %s' % err.filename
+
+def __parseACLStr__(acl_str):
+    ''' splits ACL into ACEs and parse each ACEs into a tuple of (type,flag,principle,permission) 
+    '''
+    acl = [] 
+    for ace in acl_str.split('\n'):
+        if ace:
+            acl.append(ace.split(':'))
+
+    return acl
