b51566823e54f1739469a5ce7880f96af5db6007
hurngchunlee <h.lee@donders.ru.nl>
2015-02-17 12:35:15 +0100

    creating bash wrapper for enduser tools

--- prj_wrapper.sh
+++ prj_wrapper.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 ## This wrapper script makes sure Environment module is loaded,
-## and lanuch a corresponding python executable in the directory
-## in which the wrapper is located.
+## and lanuch a corresponding python executable.
 
 me=`basename $0`
 
+## checking whether the environment module is loaded
 module list > /dev/null 2>&1
-
 if [ $? != 0 ]; then
     source /opt/_module/setup.sh
     module load cluster
-    module load python
 fi
+
+## loading module for python 2.7+
+module unload python > /dev/null 2>&1
+module load python/2.7.8
 
 myexec=$(echo $me | sed 's/prj_//').py
 
