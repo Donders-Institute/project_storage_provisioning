ade827dbd72ff3293368be9d9afaf0269e184f28
Hurng-Chun Lee <hclee.work@gmail.com>
2015-03-10 23:09:06 +0100

    update the test script

--- tests/test_acl.py
+++ tests/test_acl.py
@@ -12,10 +12,16 @@ fs = Nfs4ProjectACL(project_root, lvl)
 
 # get roles
 roles = fs.getRoles()
-
 for p, r in roles.iteritems():
     print 'path: %s' % p, r
 
 # set roles
+ick = fs.setRoles(users=['rendbru'])
+ick = fs.setRoles(path='subject_001', contributors=['rendbru'])
+
+# get roles for all sub-paths
+roles = fs.getRoles(recursive=True)
+for p, r in roles.iteritems():
+    print 'path: %s' % p, r
 
 # delete users
