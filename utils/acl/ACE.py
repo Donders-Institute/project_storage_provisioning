cab910f19e610c284d781a9294aa304dc9c9e641
hurngchunlee <h.lee@donders.ru.nl>
2018-01-06 15:07:30 +0100

    add additional boolean functions

--- utils/acl/ACE.py
+++ utils/acl/ACE.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import re
 
 class ACE:
     '''object of the Access Control Entry, the main structure is inspired by NFSv4 ACE'''
@@ -9,10 +10,19 @@ class ACE:
         self.mask = ''
         self.__dict__.update(kwargs)
 
+    def isDefaultPrinciple(self):
+        return re.match('^(OWNER|GROUP|EVERYONE)@', self.principle)
+
+    def isFileInherited(self):
+        return self.flag.find('f') >= 0
+
+    def isDirectoryInherited(self):
+        return self.flag.find('d') >= 0
+
     def __str__(self):
         return ':'.join([self.type, self.flag, self.principle, self.mask])
 
-    def __str_no_inherit__(self):
+    def __str_no_inheritance__(self):
         return ':'.join([self.type, self.flag.replace('f','').replace('d',''), self.principle, self.mask])
 
     def __repr__(self):
