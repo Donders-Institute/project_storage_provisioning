1f832cc3d5ef12f9f69f2fefef4abd6a1df192b7
hurngchunlee <h.lee@donders.ru.nl>
2018-08-07 16:10:44 +0200

    Added process lock

--- cron/db_cron-activate-project-role.sh
+++ cron/db_cron-activate-project-role.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+flock=/tmp/.activate-project-role.lock
+
+if [ -f $flock ]; then
+    echo "previous process still running ... existing"
+    exit 0
+fi
+
+touch $flock
+
 source /mnt/software/_modules/setup.sh
 module load cluster
-module load python/2.7.8
+#module load python/2.7.8
 
 ${CLUSTER_UTIL_ROOT}/external/project_acl/sbin/activate-project-role.py $*
+
+rm -f $flock
--- cron/db_cron-report-project-role.sh
+++ cron/db_cron-report-project-role.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+flock=/tmp/.report-project-role.lock
+
+if [ -f $flock ]; then
+    echo "previous process is still running ... existing"
+    exit 0
+fi
+
+touch $flock
+
 source /mnt/software/_modules/setup.sh
 module load cluster
-module load python/2.7.8
+#module load python/2.7.8
 
 ${CLUSTER_UTIL_ROOT}/external/project_acl/sbin/report-project-role.py $*
+
+rm -f $flock
